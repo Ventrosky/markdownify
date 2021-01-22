fb8727f15b02c4d261e0204d6ef99450164ed3f5
ventrosky <ventrosky@develer.com>
2021-01-22 23:35:59 +0100

    💄 add flash message

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -3,6 +3,18 @@
             [reagent.core :as r]
             ["showdown" :as s]))
 
+(defonce flash-message (r/atom nil))
+(defonce flash-timeout (r/atom nil))
+
+(defn flash 
+  ([text]
+   (flash text 3000))
+  ([text ms]
+   (js/clearTimeout @flash-timeout)
+   (reset! flash-message text)
+   (reset! flash-timeout
+           (js/setTimeout #(reset! flash-message nil) ms))))
+
 (defonce s-converter (s/Converter.))
 
 (defn md->html [md]
@@ -42,7 +54,23 @@
       (-> js/document .getSelection (.addRange selected)))))
 
 (defn app []
-  [:div
+  [:div {:style {:position :relative}}
+   [:div
+    {:style {:position :absolute
+             :margin :auto
+             :left 0
+             :right 0
+             :text-align :center
+             :max-width 200
+             :padding "2em"
+             :background-color "yellow"
+             :z-index 100
+             :border-radius 10
+             :transform (if @flash-message
+                          "scaleY(1)"
+                          "scaleY(0)")
+             :transition "transform 0.2s ease-out"}}
+    @flash-message]
    [:h1 "Markdownify"]
    [:div
     {:style {:display :flex}}
@@ -58,7 +86,9 @@
                :height "500px"
                :width "100%"}}]
      [:button
-      {:on-click #(copy-to-clipboard (->md @text-state))
+      {:on-click (fn []
+                   (copy-to-clipboard (->md @text-state))
+                   (flash "Markdown copied to clipboard"))
        :style {:background-color :green
                :padding "1em"
                :color :white
@@ -76,7 +106,9 @@
                :height "500px"
                :width "100%"}}]
      [:button
-      {:on-click #(copy-to-clipboard (->html @text-state))
+      {:on-click (fn []
+                   (copy-to-clipboard (->html @text-state))
+                   (flash "HTML copied to clipboard"))
        :style {:background-color :green
                :padding "1em"
                :color :white
